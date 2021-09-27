571bd2b19911e8fa1621de247b6ba62515b12559
Ruslan Khaliullin <ruslan_Khaliullin@epam.com>
2021-09-27 16:32:37 +0300

    Adding dependencies and creting entity "UserEntity"

--- src/main/java/com/work/ruslan/testrestful/entity/UserEntity.java
+++ src/main/java/com/work/ruslan/testrestful/entity/UserEntity.java
@@ -1,4 +1,62 @@
 package com.work.ruslan.testrestful.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String userSurname;
+    private String userThirdName;
+    private String email;
+
+    public UserEntity(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSurname() {
+        return userSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        this.userSurname = userSurname;
+    }
+
+    public String getUserThirdName() {
+        return userThirdName;
+    }
+
+    public void setUserThirdName(String userThirdName) {
+        this.userThirdName = userThirdName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
