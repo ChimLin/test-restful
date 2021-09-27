fc41a1c576260c30d79c09e8abc273dc8b4292fd
Ruslan Khaliullin <ruslan_Khaliullin@epam.com>
2021-09-27 15:57:54 +0300

    Initial commit

---
+++ src/main/java/com/work/ruslan/testrestful/TestRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.work.ruslan.testrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRestfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/work/ruslan/testrestful/TestRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.ruslan.testrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
