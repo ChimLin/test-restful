e73cb7ee31d5da7f76f14d721524eb52beabe049
Ruslan Khaliullin <ruslan_Khaliullin@epam.com>
2021-09-27 22:28:17 +0300

    Logic for wether the email is valid or not

--- src/main/java/com/work/ruslan/testrestful/service/UserService.java
+++ src/main/java/com/work/ruslan/testrestful/service/UserService.java
@@ -1,5 +1,8 @@
 package com.work.ruslan.testrestful.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.work.ruslan.testrestful.entity.UserEntity;
 import com.work.ruslan.testrestful.exception.UserNotFoundException;
 import com.work.ruslan.testrestful.repository.UserRepo;
@@ -13,8 +16,23 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
-    public UserEntity registration(UserEntity user){
+    public UserEntity registration(UserEntity user) throws IllegalArgumentException{
+        if (!isEmailValid(user.getEmail())){
+            throw new IllegalArgumentException("email is invalid");
+        }
         return userRepo.save(user);
+    }
+
+    public boolean isEmailValid(String email){
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
     }
 
     public UserEntity getOne(Long id) throws UserNotFoundException {
